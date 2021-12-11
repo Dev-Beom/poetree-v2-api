25f61f530dedcad0ef10418181ce2489a7837a79
leesilverash <leesilverash@gmail.com>
2021-12-11 16:34:41 +0900

    로그인 및 jwt 인증 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
@@ -10,13 +11,14 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
+  @Post('/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
