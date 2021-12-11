cb70d454dcde8f5ffaaef85250c97653410f1203
leesilverash <leesilverash@gmail.com>
2021-12-11 15:23:01 +0900

    로그인 인증 구현

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
+  exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService],
 })
