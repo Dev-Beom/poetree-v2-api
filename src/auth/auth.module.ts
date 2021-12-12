c1befc886d37e98a4d595e32574078116d542112
leesilverash <leesilverash@gmail.com>
2021-12-12 21:20:06 +0900

    ConfigModule 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,8 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
-import dotenv from 'dotenv';
 import { JwtStrategy } from './jwt.strategy';
 
-dotenv.config();
 @Module({
   imports: [
     UsersModule,
