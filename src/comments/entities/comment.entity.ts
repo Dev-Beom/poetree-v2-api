51802beafce475d664188fef95ffbf85e82d542f
leesilverash <leesilverash@gmail.com>
2021-12-13 23:40:33 +0900

    edit

---
+++ src/comments/entities/comment.entity.ts
@@ -0,0 +1,34 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Post } from 'src/posts/entities/post.entity';
+
+@Entity({ schema: 'crow', name: 'comments' })
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 30 })
+  code: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  name: number;
+
+  @Column({ type: 'boolean', default: 1 })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Post, (post) => post.category)
+  posts: Post[];
+}
